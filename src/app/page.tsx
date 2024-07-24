b4a7dd6aabd0cac83dd3be1ae1c6e31f3b576f86
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-07-24 12:33:24 -0300

    🎨  style: update background colors and typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 
 export default function Home() {
   return (
-    <h1 className="font-bold text-5xl text-purple">Hello Tailwind!</h1>
+    <div className="p-8 bg-slate-900 text-slate-100 h-screen">
+      <h1 className="font-bold text-5xl text-purple">Hello Tailwind!</h1>
+      <h2>Hello Tailwind!</h2>
+      <p>Hello Tailwind!</p>
+    </div>
   );
 }
