ef1b674ac3460a1adfd723cea609c4b7126c013f
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-07-31 11:08:12 -0300

    🎨 improvement: add footer in sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -4,7 +4,7 @@ import { MainNavigator } from './MainNavigation'
 
 export function Sidebar() {
   return (
-    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
       <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
@@ -16,6 +16,8 @@ export function Sidebar() {
       </div>
 
       <MainNavigator />
+
+      <div className="mt-auto flex flex-col gap-6">footer</div>
     </aside>
   )
 }
