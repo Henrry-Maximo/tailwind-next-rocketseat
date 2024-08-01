8d9387256d13cd1a395a9108b3bd2fc56fb7ecf6
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-08-01 08:48:53 -0300

    📦 refactor: remove component NavItem

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,6 +1,16 @@
-import { Search } from 'lucide-react'
+import {
+  Search,
+  BarChart,
+  CheckSquare,
+  Flag,
+  History,
+  HomeIcon,
+  LayoutDashboard,
+  SquareStack,
+  Users,
+} from 'lucide-react'
 import { Logo } from './Logo'
-import { MainNavigator } from './MainNavigation'
+import { NavItem } from './MainNavigation/Navitem'
 
 export function Sidebar() {
   return (
@@ -10,12 +20,21 @@ export function Sidebar() {
       <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
         <Search className="h-5 w-5 text-zinc-500" />
         <input
-          className="border-0bg-transparent flex-1 p-0 text-zinc-900 placeholder-zinc-600"
+          className="border-0bg-transparent flex-1 p-0 text-zinc-900 placeholder-zinc-600 outline-0"
           placeholder="Search"
         />
       </div>
 
-      <MainNavigator />
+      <nav className="space-y-0.5">
+        <NavItem title="Home" icon={HomeIcon} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+        <NavItem title="Analytics" icon={LayoutDashboard} />
+        <NavItem title="About" icon={History} />
+      </nav>
 
       <div className="mt-auto flex flex-col gap-6">footer</div>
     </aside>
