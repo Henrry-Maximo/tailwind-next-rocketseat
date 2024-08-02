e32d0c33901f3fedc424f4a88acedbc8f716ebf7
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-08-02 08:46:43 -0300

    📦refactor: sidebar in components

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -12,20 +12,15 @@ import {
   Cog,
 } from 'lucide-react'
 import { Logo } from './Logo'
-import { NavItem } from './MainNavigation/Navitem'
+import { NavItem } from './MainItem/Navitem'
+import { UsedSpaceWidget } from './MainUsedSpaceWidget/UsedSpaceWidget'
+import { SearchWidget } from './MainSearchWidget/SearchWidget'
 
 export function Sidebar() {
   return (
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
-
-      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          className="border-0bg-transparent flex-1 p-0 text-zinc-900 placeholder-zinc-600 outline-0"
-          placeholder="Search"
-        />
-      </div>
+      <SearchWidget />
 
       <nav className="space-y-0.5">
         <NavItem title="Home" icon={HomeIcon} />
@@ -44,35 +39,7 @@ export function Sidebar() {
           <NavItem title="Settings" icon={Cog} />
         </nav>
 
-        <div className="flex flex-col gap-4 rounded-lg bg-violet-50 px-4 py-5">
-          <div className="space-y-1">
-            <span className="text-sm/5 font-medium text-violet-700">
-              Used space
-            </span>
-            <p className="text-sm/5 text-violet-500">
-              Your team has used 80% of your available space. Need more?
-            </p>
-          </div>
-
-          <div className="h-2 rounded-full bg-violet-100">
-            <div className="h-2 w-4/5 rounded-full bg-violet-600"></div>
-          </div>
-
-          <div className="space-x-3">
-            <button
-              type="button"
-              className="text-sm font-medium text-violet-500 hover:text-violet-700"
-            >
-              Dismiss
-            </button>
-            <button
-              type="button"
-              className="text-sm font-medium text-violet-700 hover:text-violet-900"
-            >
-              Upgrade plan
-            </button>
-          </div>
-        </div>
+        <UsedSpaceWidget />
       </div>
     </aside>
   )
