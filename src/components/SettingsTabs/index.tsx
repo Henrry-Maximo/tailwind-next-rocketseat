51fc560821182341dff9a222464e61e34cbe809b
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-08-07 09:43:34 -0300

    ✨feat: using SettingsTabs/TabItem component / 🎨 style: add redux in componenet logic

--- src/components/SettingsTabs/index.tsx
+++ src/components/SettingsTabs/index.tsx
@@ -2,18 +2,61 @@
 'use client'
 
 import * as Tabs from '@radix-ui/react-tabs'
+import { TabItem } from './TabItem'
+import { useState } from 'react'
 
 export function SettingsTabs() {
+  const [currentTab, setCurretTab] = useState('tab1')
+
   return (
     <>
-      <Tabs.Root>
+      <Tabs.Root value={currentTab} onValueChange={setCurretTab}>
         <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200">
-          <Tabs.Trigger
-            value="my-deitals"
-            className="px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700"
-          >
-            <span className="">My details</span>
-          </Tabs.Trigger>
+          <TabItem
+            value="tab1"
+            title="My details"
+            isSelected={currentTab === 'tab1'}
+          />
+          <TabItem
+            value="tab2"
+            title="Profile"
+            isSelected={currentTab === 'tab2'}
+          />
+          <TabItem
+            value="tab3"
+            title="Password"
+            isSelected={currentTab === 'tab3'}
+          />
+          <TabItem
+            value="tab4"
+            title="Team"
+            isSelected={currentTab === 'tab4'}
+          />
+          <TabItem
+            value="tab5"
+            title="Biling"
+            isSelected={currentTab === 'tab5'}
+          />
+          <TabItem
+            value="tab6"
+            title="Email"
+            isSelected={currentTab === 'tab6'}
+          />
+          <TabItem
+            value="tab7"
+            title="Notifications"
+            isSelected={currentTab === 'tab7'}
+          />
+          <TabItem
+            value="tab8"
+            title="Integrations"
+            isSelected={currentTab === 'tab8'}
+          />
+          <TabItem
+            value="tab9"
+            title="API"
+            isSelected={currentTab === 'tab9'}
+          />
         </Tabs.List>
       </Tabs.Root>
     </>
