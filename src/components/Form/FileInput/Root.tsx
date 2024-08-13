93a2aaccfa3a79ecc5237b6bab12aa77fef297dd
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-08-13 12:34:06 -0300

    ✨ feat: adduseState with ReactContext for get a file

--- src/components/Form/FileInput/Root.tsx
+++ src/components/Form/FileInput/Root.tsx
@@ -1,7 +1,34 @@
-import { ComponentProps } from 'react'
+'use client'
 
+import { ComponentProps, createContext, useContext, useId, useState } from 'react'
+
+// Root accept elements <div> / extract props of div
 export type RootProps = ComponentProps<'div'>
 
-export function Root(props: RootProps) {
-  return <div {...props} />
+// creating context typing for input
+type FileInputContextType = {
+  id: string
+  files: File[]
+  onFilesSelected: (files: File[]) => void
 }
+
+// state of input / object null / type
+const FileInputContext = createContext({} as FileInputContextType)
+
+export function Root(props: RootProps) {
+  // hook -> user id unique
+  const id = useId();
+  const [ files, setFiles ] = useState<File[]>([]) 
+
+  // without chance of typing ´setFiles´
+
+  // disponibiliza o ID, os arquivos selecionados e a função setFiles para todos 
+  // os componentes descendentes que utilizam esse contexto.
+  return (
+    <FileInputContext.Provider value={{ id, files, onFilesSelected: setFiles }}>
+      <div {...props} />
+    </FileInputContext.Provider>
+  )
+}
+
+export const useFileInput = () => useContext(FileInputContext)
